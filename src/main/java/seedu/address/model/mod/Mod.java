9b93ee2eafb63f0295724027e42bf9031bacaa4c
nigelnnk <17807884+nigelnnk@users.noreply.github.com>
2020-03-08 23:12:34 +0800

    Update Module classes and structure as part of model section

---
+++ src/main/java/seedu/address/model/mod/Mod.java
@@ -0,0 +1,98 @@
+package seedu.address.model.mod;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+/**
+ * Represents a Module in TAble.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Mod {
+
+    // Identity fields
+    private final ModCode modCode;
+    private final String description;
+
+    // Data fields
+    private final Map<String, ModLink> links = new HashMap<>();
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Mod(ModCode modCode, String description) {
+        requireAllNonNull(modCode, description);
+        this.modCode = modCode;
+        this.description = description;
+    }
+
+    public ModCode getModCode() {
+        return modCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Returns an immutable map of description and links, which throws {@code UnsupportedOperationException}
+     * if modification is attempted.
+     */
+    public Map<String, ModLink> getLinks() {
+        return Collections.unmodifiableMap(links);
+    }
+
+    /**
+     * Returns true if both modules have the same module code.
+     * This defines a weaker notion of equality between two modules.
+     */
+    public boolean isSameMod(seedu.address.model.mod.Mod otherMod) {
+        if (otherMod == this) {
+            return true;
+        }
+
+        return otherMod != null
+                && otherMod.getModCode().equals(getModCode());
+    }
+
+    /**
+     * Returns true if both persons have the same module code, description and tags.
+     * This defines a stronger notion of equality between two persons.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof seedu.address.model.mod.Mod)) {
+            return false;
+        }
+
+        seedu.address.model.mod.Mod otherMod = (seedu.address.model.mod.Mod) other;
+        return otherMod.getModCode().equals(getModCode())
+                && otherMod.getDescription().equals(getDescription())
+                && otherMod.getLinks().equals(getLinks());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(modCode, description, links);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getModCode())
+                .append(" Description: ")
+                .append(getDescription())
+                .append(" Links: ");
+        getLinks().forEach((desc, link) -> builder.append(desc).append(" ").append(link).append("\n"));
+        return builder.toString();
+    }
+
+}
---
+++ src/main/java/seedu/address/model/mod/ModLink.java
@@ -0,0 +1,58 @@
+package seedu.address.model.mod;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Module's link to a web resource in TAble.
+ * Guarantees: immutable; is valid as declared in {@link #isValidModLink(String)}
+ */
+
+public class ModLink {
+
+    public static final String MESSAGE_CONSTRAINTS = "Module links should follow a URL link";
+
+    /*
+     * The entire string must fit the specifications for it to be valid.
+     */
+    public static final String VALIDATION_REGEX =
+            "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]$";
+
+    public final String modLink;
+
+    /**
+     * Constructs a {@code ModLink}.
+     *
+     * @param modLink A valid module code.
+     */
+    public ModLink(String modLink) {
+        requireNonNull(modLink);
+        checkArgument(isValidModLink(modLink), MESSAGE_CONSTRAINTS);
+        this.modLink = modLink;
+    }
+
+    /**
+     * Returns true if a given string is a valid tutorial name.
+     */
+    public static boolean isValidModLink(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return modLink;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof seedu.address.model.mod.ModLink // instanceof handles nulls
+                && modLink.equals(((seedu.address.model.mod.ModLink) other).modLink)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return modLink.hashCode();
+    }
+}
