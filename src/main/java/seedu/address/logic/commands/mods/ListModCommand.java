5cb7fc3ecfb227622a66a3504f1d6c6c74ee50d4
CranstonYeo <e0310976@u.nus.edu>
2020-03-24 18:39:04 +0800

    Update GUI and allow list commands to open the corresponding tabs

--- src/main/java/seedu/address/logic/commands/mods/ListModCommand.java
+++ src/main/java/seedu/address/logic/commands/mods/ListModCommand.java
@@ -20,6 +20,6 @@ public class ListModCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredModList(PREDICATE_SHOW_ALL_MODS);
-        return new CommandResult(MESSAGE_SUCCESS);
+        return new CommandResult(MESSAGE_SUCCESS, false, false, true, false);
     }
 }
