ece39e8d276ce485da0431d037d5823424e8f842
hanchingyong <hanchingyong@gmail.com>
2020-03-28 20:46:54 +0800

    delete consult test

--- src/test/java/seedu/address/logic/commands/consult/AddConsultCommandTest.java
+++ src/test/java/seedu/address/logic/commands/consult/AddConsultCommandTest.java
@@ -1,4 +1,4 @@
-package seedu.address.logic.commands;
+package seedu.address.logic.commands.consult;
 
 import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertFalse;
