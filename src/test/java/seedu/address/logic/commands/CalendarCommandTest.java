85e4261f817dd411342cbaac7f5b43eed1095d19
hanchingyong <hanchingyong@gmail.com>
2020-03-28 21:38:21 +0800

    calendar command test

---
+++ src/test/java/seedu/address/logic/commands/CalendarCommandTest.java
@@ -0,0 +1,19 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CalendarCommand.SHOWING_CALENDAR_MESSAGE;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+
+public class CalendarCommandTest {
+    private Model model = new ModelManager();
+    private Model expectedModel = new ModelManager();
+
+    @Test
+    public void execute_calendar_success() {
+        CommandResult expectedCommandResult = new CommandResult(SHOWING_CALENDAR_MESSAGE, true, false, false, false);
+        assertCommandSuccess(new CalendarCommand(), model, expectedCommandResult, expectedModel);
+    }
+}
